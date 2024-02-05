c635772bc113a8dd7e0c5edaef96cd8ea5862372
WayneRoseberryGit <wayne_roseberry@hotmail.com>
2024-02-04 17:07:57 -0800

    implemented randomedecider

--- datatools.datamakerjs/datamakerlibs/randomDecider.js
+++ datatools.datamakerjs/datamakerlibs/randomDecider.js
@@ -1,9 +1,40 @@
-﻿class RandomDecider {
+﻿const { MUSTBESINGLECHARACTERERROR, MAXALPHMUSTBEGREATERTHANOREQUALTOMINALPHA, MAXNUMERICMUSTBEGREATERTHANOREQUALTOMINNUMERIC } = require("./commonschema");
+
+class RandomDecider {
     constructor() {
-        this.chooseAlphaRange = function(minAlpha, maxAlpha) { throw "not implemented"; };
-        this.chooseNumericRange = function(minNumeric, maxNumeric) { throw "not implemented"; };
-        this.chooseItem = function(itemArray) { throw "not implemented"; };
-        this.optionChosen = function(schemaObject) { throw "not implemented"; };
+        this.chooseAlphaRange = function (minAlpha, maxAlpha) {
+            if (minAlpha.length > 1 || maxAlpha.length > 1) {
+                throw MUSTBESINGLECHARACTERERROR;
+            }
+            if (maxAlpha < minAlpha) {
+                throw MAXALPHMUSTBEGREATERTHANOREQUALTOMINALPHA;
+            }
+
+            var minVal = minAlpha.charCodeAt(0);
+            var maxVal = maxAlpha.charCodeAt(0);
+            var chosenNumber = pickRandomNumberInRange(maxVal, minVal);
+            return String.fromCharCode(chosenNumber);
+        };
+        this.chooseNumericRange = function (minNumeric, maxNumeric) {
+            if (maxNumeric < minNumeric) {
+                throw MAXNUMERICMUSTBEGREATERTHANOREQUALTOMINNUMERIC;
+            }
+            return pickRandomNumberInRange(maxNumeric, minNumeric);
+        };
+        this.chooseItem = function (itemArray) {
+            if (itemArray.length < 1) {
+                throw "item array must not be empty"
+            }
+
+            var maxItem = itemArray.length;
+            var chosen = Math.floor(Math.random() * maxItem);
+            return itemArray[chosen];
+        };
+        this.optionChosen = function(schemaObject) { return Math.random() > 0.5 ; };
     }
 }
-exports.DeciderMock = RandomDecider;
+exports.RandomDecider = RandomDecider;
+
+function pickRandomNumberInRange(maxVal, minVal) {
+    return Math.floor(Math.random() * (maxVal - minVal + 1)) + minVal;
+}
